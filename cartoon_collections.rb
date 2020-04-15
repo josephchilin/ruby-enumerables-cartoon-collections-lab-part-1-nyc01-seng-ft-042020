7b181065f37b53f3914106fcb5c6654b946dedf8
Jo L <joseph@josephchilin.com>
2020-04-15 22:56:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,7 @@ def list_dwarves(array)
   array.each_with_index do |name, index|
     array[name] = index
     
-    puts "#{index+1}. #{name}"
+    puts "#{index.to_i+1}. #{name}"
     
   end
 end
